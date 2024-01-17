419229def3887fb520bae43fee6304d011827189
namurice <102151452+namurice@users.noreply.github.com>
2024-01-18 01:44:40 +0400

    Add files via upload

---
+++ EmailAddress.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalsProject.Validations
+{
+    public class EmailAddress : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Email address is required");
+            }
+            if (value is string email) 
+            {
+                if (email.Contains('@') && email.Contains('.'))
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult("Invalid");
+                }
+            }
+            return new ValidationResult("Email Address must be a string");
+        }
+    }
+}
---
+++ Phone.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalsProject.Validations
+{
+    public class Phone : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Phone number is required");
+            }
+            if (value is string phone)
+            {
+                long number;
+                if (long.TryParse(phone, out number))
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult("Invalid");
+                }
+            }
+            else
+            {
+                return new ValidationResult("Invalid");
+            }
+        }
+    }
+}
