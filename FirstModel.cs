924a2d3599871bf8a698e34e6bf512c9a4761f9f
namurice <102151452+namurice@users.noreply.github.com>
2024-01-18 01:36:40 +0400

    Add files via upload

---
+++ FirstModel.cs
@@ -0,0 +1,16 @@
+﻿using FinalsProject.Validations;
+
+namespace FinalsProject.Models
+{
+    public class FirstModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Phone]
+        public string PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
